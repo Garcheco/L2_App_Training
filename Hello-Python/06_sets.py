adb615c140859678b7f47738a6f0606fd8b68c49
Garcheco <64005277+Garcheco@users.noreply.github.com>
2024-03-03 05:46:05 +0000

    modified:   Hello-Python/04_lists.py 	new file:   Hello-Python/05_tuples.py 	new file:   Hello-Python/06_sets.py

---
+++ Hello-Python/06_sets.py
@@ -0,0 +1,10 @@
+### Sets ###
+
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set)) # Inicialmente es un diccionario
+
+my_other_set = {"Brais", "Moure", 35}
+print(type(my_other_set))
