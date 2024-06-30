3f8da2e44a3ce5b64722541c28367051fe112b05
Garcheco <64005277+Garcheco@users.noreply.github.com>
2024-06-30 01:11:12 +0000

    modified:   Bash/Udemy/123.sh 	new file:   Bash/Udemy/Project/000_hello.txt 	new file:   Bash/Udemy/Project/00_hello.txt 	new file:   Bash/Udemy/Project/01_variables.txt 	new file:   Bash/Udemy/Project/02_operators.txt 	new file:   Bash/Udemy/Project/03_strings.txt 	new file:   Bash/Udemy/Project/04_lists.txt 	new file:   Bash/Udemy/Project/05_tuples.txt 	new file:   Bash/Udemy/Project/06_sets.txt 	new file:   Bash/Udemy/Project/1.txt 	new file:   Bash/Udemy/Project/123.sh 	new file:   Bash/Udemy/Project/2.txt 	new file:   Bash/Udemy/Project/3.txt 	new file:   Bash/Udemy/Project/4.txt 	new file:   Bash/Udemy/Project/5.txt 	new file:   Bash/Udemy/Project/6.txt 	new file:   Bash/Udemy/Project/ck_f1.jpg 	new file:   Bash/Udemy/Project/ck_f2.jpg 	new file:   Bash/Udemy/Project/ck_f3.jpg 	new file:   Bash/Udemy/Project/ck_f4.jpg 	new file:   Bash/Udemy/Project/ck_f5.jpg 	new file:   Bash/Udemy/Project/ck_f6.jpg 	new file:   Bash/Udemy/Project/hot-folder/000_hello.txt 	new file:   Bash/Udemy/Project/hot-folder/ourscript1.sh 	new file:   Bash/Udemy/Project/hot-folder/ourscript2.sh 	new file:   Bash/Udemy/Project/hot-folder/v_exercise_3.sh 	new file:   Bash/Udemy/Project/ourscript1.sh 	new file:   Bash/Udemy/Project/ourscript2.sh 	new file:   Bash/Udemy/Project/v_exercise_3.sh

---
+++ Bash/Udemy/Project/hot-folder/ourscript2.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+
+for i in $(find . -type d);
+do
+
+    if [ "$i" != "./hot-folder" ]; then
+
+    echo "*********** FOLDER: $i *************"
+
+    for myfile in $i/* ;
+    do
+        if [ -f "$myfile" ]; then
+            echo "the file inside is: $myfile"
+
+            # DO WHATEVER YOU WANT HERE..
+            #(BUT BE CAREFUL!!!) FOR EX: WE CAN DELETE, CREATE, FETCH....
+            check=$(grep -ni "if" "$myfile")
+		    if [ -z "$check" ]; then
+			    echo "EMPTY"
+		    else
+			    echo "HOT FILE! FOUND !"
+
+                name_f=$(basename $myfile)
+                cp "$myfile" ./hot-folder
+                echo " " >> hot-folder/"$name_f"
+                echo "********" >> hot-folder/"$name_f"
+                echo "$check" >> hot-folder/"$name_f"
+		    fi
+        fi
+        
+    done
+    echo " ------------------ "
+    echo " "
+    fi
+
+done
+ 
+********
+7:    if [ "$i" != "./hot-folder" ]; then
+13:        if [ -f "$myfile" ]; then
+18:            check=$(grep -ni "if" "$myfile")
+19:		    if [ -z "$check" ]; then
